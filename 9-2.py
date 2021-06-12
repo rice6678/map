ec13f3bb91be18031634fb7a35c9c2b1912b260e
rice6678 <85775421+rice6678@users.noreply.github.com>
2021-06-12 12:56:19 +0900

    Add files via upload

--- 9-2.py
+++ 9-2.py
@@ -6,6 +6,6 @@ m=folium.Map([36.509966169639654, 127.2659707123968], zoom_start=15)
 folium.Marker([36.50836270150055, 127.26884668756118], popup='학교').add_to(m)
 folium.Marker([36.511481685979106, 127.26299314522721], popup='초등학교').add_to(m)
 folium.Marker([36.509984857079395, 127.26629368754531], popup='우리집').add_to(m)
-
+folium.Marker([37.241449588374174, 131.86477881014255], popup='독도').add_to(m)
 m.save('index.html')
 webbrowser.open('index.html')
